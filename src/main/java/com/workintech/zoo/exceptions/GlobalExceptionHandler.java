1c495e98b77f7be438acd8e8e2ee3b7e51b16407
hmzshin <hamzasahin@outlook.com>
2024-02-07 22:17:17 +0300

    create GlobalExceptionHandler to manage Exceptions globally

---
+++ src/main/java/com/workintech/zoo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.workintech.zoo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(KangarooException.class)
+    public ResponseEntity<ErrorResponse> handleKangarooException(KangarooException kangarooException) {
+        ErrorResponse errorResponse =
+                new ErrorResponse(kangarooException.getStatus().value(),
+                        kangarooException.getMessage(),
+                        LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, kangarooException.getStatus());
+    }
+
+    @ExceptionHandler(KoalaException.class)
+    public ResponseEntity<ErrorResponse> handleKoalaException(KoalaException koalaException) {
+        ErrorResponse errorResponse =
+                new ErrorResponse(koalaException.getStatus().value(),
+                        koalaException.getMessage(),
+                        LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, koalaException.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse errorResponse =
+                new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                        exception.getMessage(),
+                        LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
